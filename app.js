7acf4d76979bd278bea580ad4502e0f640309edd
Robyn Berkel <robynberkel@pal-nat186-39-210.itap.purdue.edu>
2016-04-20 16:04:12 -0400

    trying to connect to the database

--- app.js
+++ app.js
@@ -21,6 +21,25 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+//connect to the database
+var ibmdb = require('ibm_db');
+connstring = "DRIVER={DB2};DATABASE=<SQL Database-t2>;HOSTNAME=<75.126.155.153>;UID=user04848;PWD=hjC8gSiSTzR1;PORT=<50000>;PROTOCOL=TCPIP";
+ibmdb.open(connstring, function(err,data) {
+	if (err) return console.log(err);
+	//collect the collaborations from the database
+	conn.query('select collaboration, position, picture from Images ORDER BY collaboration, position') {
+		if (err) console.log(err);
+		else console.log(data);
+
+		conn.close(function() {
+			console.log('done');
+		});
+	});
+});
+
+//parse the results of the query
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
