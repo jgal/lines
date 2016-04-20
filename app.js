758f2de951dd6644cc559cb80c81b5cd12dd7fee
Robyn Berkel <rberkel@purdue.edu>
2016-04-20 20:46:54 +0000

    re-adding change round 4

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 //connect to the database
-/*var ibmdb = require('ibm_db'); 
+var ibmdb = require('ibm_db'); 
 var connstring = "DRIVER={DB2};DATABASE=<SQL Database-t2>;HOSTNAME=<75.126.155.153>;UID=user04848;PWD=hjC8gSiSTzR1;PORT=<50000>;PROTOCOL=TCPIP";
 ibmdb.open(connstring, function(err,conn) {
 	if (err) return console.log(err);
@@ -37,7 +37,7 @@ ibmdb.open(connstring, function(err,conn) {
 		});
 	});
 });
-*/
+
 //parse the results of the query
 
 
