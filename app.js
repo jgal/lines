5892044afb9e31c4cc787651877d2bce2f7426c5
Robyn Berkel <rberkel@purdue.edu>
2016-04-21 20:34:53 +0000

    connecting to database

--- app.js
+++ app.js
@@ -22,16 +22,18 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 //connect to the database
-/*var ibmdb = require('ibm_db'), 
- connstring = "DATABASE=SQL Database-t2;HOSTNAME=75.126.155.153;UID=user04848;PWD=hjC8gSiSTzR1;PORT=50000;PROTOCOL=TCPIP;";
-ibmdb.open(connstring, function(err,conn) {
+var ibmdb = require('ibm_db');
+	ibmdb.open("DRIVER={DB2};DATABASE=SQLDB;HOSTNAME=<\"75.126.155.153\">;UID=<\"user04848\">;PWD=<\"hjC8gSiSTzR1\">;PORT=<\"50000\">;PROTOCOL=TCPIP", function (err,conn) {
+	if (err) console.log(err);
 	//collect the collaborations from the database
 	var query = "select collaboration, position, picture from Images ORDER BY collaboration, position";
 	var rows = conn.querySync(query);
 	console.log(rows);
 		conn.close(function() {
 			console.log('done');
-});*/
+		});
+
+});
 
 //parse the results of the query
 
