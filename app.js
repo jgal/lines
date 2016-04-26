c0da0f6b98b926b985c5e612c49555a35234282d
Robyn Berkel <rberkel@purdue.edu>
2016-04-26 06:11:19 +0000

    adding app

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var http = require('http');
 var path = require('path');
 var ibmdb = require('ibm_db');
 
+var app = express();
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
