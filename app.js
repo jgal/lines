09f8738427583c4f5fd6327efd5a444d62bc8c73
jgal <jgal@purdue.edu>
2016-04-25 21:51:53 -0400

    set views directory to /public

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var app = express();
 app.set('view engine', 'ejs');
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-//app.set('views', __dirname + '/public')
+//app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public')
 
 app.get('/dummy', function(request, response) {
   response.render('dummy');
